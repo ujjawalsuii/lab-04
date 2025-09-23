8e328fc98a1d0dee2a2ae44e17f1f39866bbf6eb
Ujjawal Singh <ujjawalpratap1234@gmail.com>
2025-09-23 17:52:17 -0600

    Initial

---
+++ app/src/main/java/com/example/aurora/Shape.java
@@ -0,0 +1,34 @@
+package com.example.aurora;
+
+public abstract class Shape {
+    protected int x;
+    protected int y;
+
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+
+    public abstract double getArea();
+
+    public abstract double getPerimeter();
+}
